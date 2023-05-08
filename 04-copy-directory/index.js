3c2c8b9cd5e46e4e8160730a682df710bd550126
AlexeiFilimonov <nlifeaf@mail.ru>
2023-05-08 19:36:07 +0300

    refactor: add copyDir function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,15 +4,18 @@ const dest = path.join(__dirname, 'files-copy');
 const src = path.join(__dirname, 'files');
 
 function createDir() {
-  return mkdir(dest, { recursive: true }).then(() => copyFiles());
+  mkdir(dest, { recursive: true }).then(() => copyFiles());
 }
 
 function copyFiles() {
-  return readdir(src, { withFileTypes: true }).then((files) => {
+  readdir(src, { withFileTypes: true }).then((files) => {
     files.forEach((file) => {
       copyFile(path.join(src, file.name), path.join(dest, file.name));
     });
   });
 }
 
-rm(dest, { force: true, recursive: true }).then(() => createDir());
+function copyDir() {
+  rm(dest, { force: true, recursive: true }).then(() => createDir());
+}
+copyDir();
